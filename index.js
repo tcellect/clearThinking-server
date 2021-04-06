75f3e24f9b9cb89013f11ef862dd7fd60864470a
Deniss Orlov <aristarxus@Denisss-MacBook-Pro.local>
2021-04-06 20:24:02 +0300

    prepare for heroku deployment

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use(express.urlencoded({ limit: "1mb", extended: true}));
 app.use(cors());
 app.use("/bias", biasRouts)
 
+app.get("/", (req, res) => {
+    res.send("check your thinking for biases")
+})
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
