c41c15ba5b99091901782efb0b110499e3d564e9
Deniss Orlov <aristarxus@Denisss-MacBook-Pro.local>
2021-04-07 01:29:01 +0300

    putting heroku alive

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import biasRouts from "./routes/biasR.js"
 import dotenv from "dotenv"
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 dotenv.config();
 
@@ -15,8 +17,6 @@ app.use("/bias", biasRouts)
 app.get("/", (req, res) => {
     res.send("check your thinking for biases")
 })
-
-const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`server is running on port: ${PORT}`)))
